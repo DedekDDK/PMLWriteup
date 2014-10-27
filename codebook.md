5b83c246edeee35e64c3d215b319ed2d7c1004a0
David Konvalina <dkonvalina@email.cz>
2014-10-27 01:34:04 +0100

    with html gh

---
+++ codebook.md
@@ -0,0 +1,25 @@
+Codebook
+====================
+Welcome to the codebook for Machine Learning Excercise I created during the Coursera project.
+
+###Data
+The source data came from http://groupware.les.inf.puc-rio.br/har site and contains various measurements when couple
+of people were lifting weights and were doing these excersices either properly (classe=A) or with common mistakes (classe=B ..)
+
+###Data preparation & Analysis
+Before processing the data, data from the file pml-training.csv were stripped of the line number and name of the lifter, as 
+ther are not relevant for the predictions.
+Classe (resulting factor variable) was converted to a factor.
+
+This "training" dataset was split into real train and test datasets, with p=3/4.
+All columns containing NAs were removed, so resulting number of columns in the training set was 91 (incl. classe).
+
+Using train function, the prediction model was calculated, using defaults - Random Forest option, with default boosting for cross-validation, to select best prediction model.
+
+Accuracy of the predictor on Training data is:
+Accurace of the predictor on Testing data is:
+
+Resulting predictor was used to predict data in the file pml-testing.csv, which was provided without classe value.
+These predicted values are to be submitted in the form of 20 separate files, according the result vector.
+
+
---
+++ predictions.R
@@ -0,0 +1,60 @@
+#rm(list=ls())
+library(caret)
+
+#read training data set
+trainall<-read.csv("pml-training.csv",stringsAsFactors=FALSE)
+#row number (X) and user_name are irrelevant for training
+trainall<-trainall[,-(1:5)]
+#classe to be a factor
+trainall$classe<-factor(trainall$classe)
+
+#do the same operations for predicting data set
+test<-read.csv("pml-testing.csv",stringsAsFactors=FALSE)
+#row number (X) and user_name are irrelevant for training
+test<-test[,-(1:5)]
+#there is no classe column in the "testing" data set (predicting), so no factorization needed
+
+#split the training data to train and test sets
+set.seed(230570)
+inTrain = createDataPartition(trainall$classe, p = 3/4, list=FALSE)
+training = trainall[inTrain,]
+testing = trainall[-inTrain,]
+
+#check if we have some NAs in Y
+stopifnot(complete.cases(training$classe))
+
+#select only the columns, where are no NAs and non-character cols
+oktrain<-training[,!sapply(training,function(x) any(is.na(x)))]
+oktrain<-oktrain[,!sapply(oktrain,is.character)]
+okcols<-colnames(oktrain)
+
+#use the same columns for testing data set, for validation
+oktest<-testing[,okcols]
+
+#use the same columns for the predicting data set (pml-testing.csv)
+okPredictcols<-okcols[-grep("classe",okcols)]
+okPredict<-test[,okPredictcols]
+
+
+#check if there are not some undefined cases
+stopifnot(complete.cases(oktrain))
+stopifnot(complete.cases(oktest))
+stopifnot(complete.cases(okPredict))
+
+
+#train the model of random forest with all defaults
+set.seed(232323)
+mod<-train(classe ~ ., data=oktrain)
+
+#find the Accuracy of the predictor on Train data
+predTrain<-predict(mod,oktrain)
+confusionMatrix(predTrain,oktrain$classe)
+
+#predict Testing data to get accuracy on test data set
+predTest<-predict(mod,oktest)
+confusionMatrix(predTest,oktest$classe)
+
+#finally predict the data wanted 20 cases (I call it predicting data set)
+predPredict<-predict(mod,okPredict)
+
+#predPredict is a vector for submission
