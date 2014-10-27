b84186fe52b730948a1e5db14e8839959059b9eb
David Konvalina <dkonvalina@email.cz>
2014-10-27 02:41:32 +0100

    Final use for evaluation

--- predictions.R
+++ predictions.R
@@ -58,3 +58,14 @@ confusionMatrix(predTest,oktest$classe)
 predPredict<-predict(mod,okPredict)
 
 #predPredict is a vector for submission
+
+pml_write_files = function(x){
+        n = length(x)
+        for(i in 1:n){
+                filename = paste0("problem_id_",i,".txt")
+                write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+        }
+}
+#script should work with character vector, so better to convert it
+predStr<-as.character(predPredict)
+pml_write_files(predStr)
